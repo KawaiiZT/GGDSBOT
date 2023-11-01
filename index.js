03eeb9871258d8ac0349cf09f29eecd0ad822cb3
KawaiiZT <Trisit.93@gmail.com>
2023-11-01 20:51:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const server = http.createServer(app);
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+  })
 
 app.get("/",(req, res) =>{
     res.render('index', {bot: settings.website})
@@ -13,6 +18,16 @@ app.get("/commands", (req, res) => {
     res.render("commands", {bot: settings.website, commands: settings.commands})
 })
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+  })
+  
+  app.get('/commands', (req, res) => {
+    res.send('This is my about route..... ')
+  })
+
 const listener = server.listen(8000, function() {
     console.log("Your app is listening on port" + listener.address().port);
 })
+
+module.exports = app
